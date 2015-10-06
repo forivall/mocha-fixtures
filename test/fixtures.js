c8564b516c39adfc236df855a8a6888f5af4f740
Jordan Klassen <jordan@klassen.me.uk>
2015-10-06 12:34:15 -0700

    Add tests

---
+++ test/fixtures.js
@@ -0,0 +1,40 @@
+var _ = require('lodash');
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+var buildFixtures = require('../index');
+
+var rootPath = path.resolve(__dirname, '..');
+
+function pathCustomizer(value) {
+  if (_.isString(value) && value.startsWith(rootPath)) {
+    return 'MOCHA_FIXTURES' + value.slice(rootPath.length);
+  }
+}
+
+function tryRequireOrWrite(modulePath, data) {
+  try {
+    return require(modulePath);
+  } catch(e) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      data = _.cloneDeep(data, pathCustomizer);
+      fs.writeFileSync(path.resolve(__dirname, modulePath), JSON.stringify(data, null, '  '));
+      return data;
+    }
+    throw e;
+  }
+}
+
+function testFixture(fixtureName) {
+  test(fixtureName, function () {
+    var actual = buildFixtures(__dirname + '/fixtures/' + fixtureName + '/actual');
+    var expected = tryRequireOrWrite('./fixtures/' + fixtureName + '/expected.json', actual);
+    expect(_.cloneDeep(actual, pathCustomizer)).to.deep.equal(expected);
+  });
+}
+
+suite('mocha-fixtures', function () {
+  suite('buildFixtures', function () {
+    _.forEach(fs.readdirSync(__dirname + '/fixtures'), testFixture);
+  });
+});
